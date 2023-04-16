a0ab29faa464eb8e439bde27b176c5393d7f45ee
dongnbka2 <dongdvhe141083@fpt.edu.vn>
2023-04-17 00:03:46 +0700

    logout admin

--- SWP391/src/java/controller/User/LogoutController.java
+++ SWP391/src/java/controller/User/LogoutController.java
@@ -6,10 +6,11 @@ package controller.User;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
@@ -55,7 +56,9 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("login");
     }
 
     /**
